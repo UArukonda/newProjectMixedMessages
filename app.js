286d68122f04b170428aafb3e03e77a887708eae
UArukonda <109522168+UArukonda@users.noreply.github.com>
2024-02-16 21:52:58 +0000

    First Commit

---
+++ app.js
@@ -0,0 +1,52 @@
+// MIXED MESSAGES
+// define object of arrays
+const mixedMessages = {
+  greeting: [
+    "Hello",
+    "Hi there",
+    "Greetings",
+    "Good morning",
+    "Hey",
+    "Howdy",
+    "Hiya",
+    "Salutations",
+    "Yo",
+    "Hi friend",
+  ],
+  subjects: [
+    "world",
+    "friend",
+    "there",
+    "you",
+    "universe",
+    "stranger",
+    "dreamer",
+    "adventurer",
+    "soulmate",
+    "companion",
+  ],
+  jokes: [
+    "Do you know why the bicycle couldn't stand up by itself? Because it was two-tired!",
+    "Do you know what the pirate said on his 80th birthday? Aye Matey!",
+    "Do you know what's orange and sounds like a parrot? A carrot!",
+    "Do you know why the math book looked sad? Because it had too many problems.",
+    "Do you know why the tomato turned red? Because it saw the salad dressing!",
+    "Do you know what's brown and sticky? A stick!",
+    "Do you know why the scarecrow won an award? Because he was outstanding in his field!",
+    "Do you know why the golfer brought two pairs of pants? In case he got a hole in one!",
+    "Do you know why the tomato blushed? Because it saw the salad dressing!",
+    "Do you know what's green, fuzzy, and if it fell out of a tree, it would kill you? A pool table.",
+  ],
+};
+
+// function to generate random message
+
+function generateRandomMessage(messageArrays) {
+  const randomGreet =
+    messageArrays.greeting[
+      Math.floor(Math.random() * messageArrays.greeting.length)
+    ];
+  console.log(randomGreet);
+}
+
+generateRandomMessage(mixedMessages);
